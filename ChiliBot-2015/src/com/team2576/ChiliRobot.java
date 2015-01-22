00d9dda2d72d28ca3806086ed7371bfaf3c1388b
vectorcrumb <lucas.alvarez@corazondechileno.cl>
2015-01-22 02:26:30 -0300

    First COMMIT w/ChiliRobot.java

---
+++ ChiliBot-2015/src/com/team2576/ChiliRobot.java
@@ -0,0 +1,30 @@
+package com.team2576;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class ChiliRobot extends IterativeRobot {
+	
+	public ChiliRobot() {
+		
+	}
+	
+    public void robotInit() {
+
+    }
+    
+    public void autonomousInit() {
+    	
+    }
+
+    public void autonomousPeriodic() {
+
+    }
+
+    public void teleopInit() {
+    	
+    }
+    
+    public void teleopPeriodic() {
+        
+    }
+}
