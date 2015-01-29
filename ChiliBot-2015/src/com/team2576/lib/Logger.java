f12413572c590ba520556106f4ac53befa23e51c
vectorcrumb <lucas.alvarez@corazondechileno.cl>
2015-01-29 01:26:08 -0300

    ADDED LOGGER NOW...I'M A FOOL
    lucash

---
+++ ChiliBot-2015/src/com/team2576/lib/Logger.java
@@ -0,0 +1,92 @@
+package com.team2576.lib;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class Logger {
+	
+	private String directory = "/logs";
+	private String file_path;
+	private Date logger_time;
+	private SimpleDateFormat time_format;
+	private BufferedWriter writer;
+	private int index;
+	private static Logger instance;
+	private DriverStation driver;
+	
+	private String loggables = "time,frontLeftForce,rearLeftForce,frontRightForce,rearRightForce,winchForce,"
+								+ "batteryVoltage,pdpTemp,pdpTotalCurrent,current0,current1,current2,current3,"
+								+ "current4,current5,current6,current7,";
+	
+	public static Logger getInstance() {
+		if (instance == null) {
+			instance = new Logger();
+		}
+		return instance;
+	}
+	
+	public enum Messages {
+		SENSOR_LOG,
+		OUTPUT_LOG,
+		DRIVER_LOG
+	}
+	
+	private Logger() {
+		this.driver = DriverStation.getInstance();
+		this.logger_time = new Date();
+		time_format = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+		
+		File dir = new File(this.directory);
+		if(!dir.exists()) {
+			dir.mkdir();
+		}
+		File[] files = new File(directory).listFiles();
+		if(files != null) {
+			for(File file : files) {
+				if(file.isFile()) {
+					try {
+						int count = Integer.parseInt(file.getName().split("_")[0]);
+						if (count > this.index) {
+							index = count;
+						}
+					} catch (Exception err) {
+						err.printStackTrace();
+					}
+				}
+			}
+		} else {
+			this.index = 0;
+		}
+	}
+	
+	public void openLog() {
+		try {
+			file_path = this.generatePath();
+			this.writer = new BufferedWriter(new FileWriter(this.file_path));
+			this.writer.write(this.loggables);
+			this.writer.newLine();
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+	}
+	
+	private String generatePath() {		
+		if(this.driver.isFMSAttached()) {
+			return String.format("%s/%d_%s_%s_position%d_log.txt", this.directory, ++this.index, this.generateTimeStamp(logger_time),
+								 this.driver.getAlliance().toString(), this.driver.getLocation());
+		} 
+		return String.format("%s/%d_log.txt", this.directory, ++this.index);
+	}
+
+	private String generateTimeStamp(Date time) {
+		time = null;
+		time = new Date();
+		return (time_format.format(time)).toString();
+	}
+}
